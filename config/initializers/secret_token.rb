531eca3eb596f6c60a6d633921b5779525a22daf
mpellonc <marina.consunji@gmail.com>
2015-11-07 17:51:22 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CumplePapaV2::Application.config.secret_token = 'df7dc30ec2bb8b8d990548c6df9a5ddc07b5af3eb87aee45db64253d741f64b52c96c80b5c25e7f2c2a6610cc2a4dda3a45d498d1137888fe2ad453c25360e53'
